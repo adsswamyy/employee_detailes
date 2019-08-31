14b2cb5c9ec2bc9dce081df6a61eddabe9e7a818
swamy321 <adsswamy7@gmail.com>
2019-08-31 17:53:36 +0530

    Get api implements validation and testing employees

--- employee_info/emp_app/api/tests.py
+++ employee_info/emp_app/api/tests.py
@@ -18,3 +18,14 @@ class employees_APITestCase(APITestCase):
         response=self.client.post(url,data,format='json')
         print(response.status_code)
         self.assertEqual(response.status_code,201)
+    def setup(self):
+        employees.objects.create(emp_no='01', birth_date="1990-01-05", first_name='swamy', last_name='ads',gender='male', hire_date="2019-01-05")
+
+    def test_get_method_sucuess(self):
+        url = 'http://127.0.0.1:8000/emp_app/get_api/'
+        response=self.client.get(url,data,format='json')
+        self.assertEqual(response.status_code, 201)
+        qs=employees.objects.filter(first_name='swamy')
+        self.assertEqual(qs,count(),1)
+
+
